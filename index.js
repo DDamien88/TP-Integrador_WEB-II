ac9d80acb2ad762d20864f360f25b20f0eb9b2db
DamiánD <deramodamian@gmail.com>
2024-09-22 21:51:07 -0300

    update index.js

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
-/*app.get('/', (req, res) => {
-    res.send('./public/index.html');
-});*/
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 
 app.listen(port, () => {
     console.log(`Server is running on port:${port}`);
