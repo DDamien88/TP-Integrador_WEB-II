17a59f4a0850cc26f538ccfe94d94c744e1f229e
DamiánD <deramodamian@gmail.com>
2024-09-22 21:46:13 -0300

    get comentado

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
     res.send('./public/index.html');
-});
+});*/
 
 app.listen(port, () => {
     console.log(`Server is running on port:${port}`);
